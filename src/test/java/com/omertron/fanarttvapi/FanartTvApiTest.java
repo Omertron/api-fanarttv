63f0b0e296f1750ca96fe13acd21b80e6120d18b
Stuart Boston <Stuart.Boston@Gmail.com>
2013-09-05 21:34:16 +0100

    removed unused import

--- src/test/java/com/omertron/fanarttvapi/FanartTvApiTest.java
+++ src/test/java/com/omertron/fanarttvapi/FanartTvApiTest.java
@@ -25,7 +25,6 @@ import java.util.List;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
